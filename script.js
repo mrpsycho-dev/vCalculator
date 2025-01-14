ffa045c356debd72c29556c9b5e325820acede9c
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-14 19:37:17 +0500

    more configurations

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-"use strict";
+'use strict';
 const buttonValues = [
-  "AC",
-  "+/-",
-  "%",
-  "÷",
-  "7",
-  "8",
-  "9",
-  "×",
-  "4",
-  "5",
-  "6",
-  "-",
-  "1",
-  "2",
-  "3",
-  "+",
-  "0",
-  ".",
-  "=",
+  'AC',
+  '+/-',
+  '%',
+  '÷',
+  '7',
+  '8',
+  '9',
+  '×',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '0',
+  '.',
+  '=',
 ];
+const rightSymbols = ['÷', '×', '-', '+', '='];
+const topSymbols = ['AC', '+/-', '%'];
+const display = document.querySelector('.display');
 
-const rightSymbols = ["÷", "×", "-", "+", "="];
-const topSymbols = ["AC", "+/-", "%"];
+let a = 0;
+let b = null;
+let operator = null;
 
 for (let i = 0; i < buttonValues.length; i++) {
   // Create buttons
   let value = buttonValues[i];
-  let button = document.createElement("button");
+  let button = document.createElement('button');
   button.innerText = value;
   // Span 0 button
-  if (value === "0") {
-    button.style.width = "200px";
-    button.style.gridColumn = "1/3";
+  if (value === '0') {
+    button.style.width = '200px';
+    button.style.gridColumn = '1/3';
   }
   if (rightSymbols.includes(value)) {
     // Styling buttons
-    button.style.backgroundColor = "#FF9500";
+    button.style.backgroundColor = '#FF9500';
   } else if (topSymbols.includes(value)) {
-    button.style.backgroundColor = "#D4D4D2";
-    button.style.color = "#1c1c1c";
+    button.style.backgroundColor = '#D4D4D2';
+    button.style.color = '#1c1c1c';
   }
 
   // Add created buttons in HTML
-  document.querySelector(".buttons").appendChild(button);
+  document.querySelector('.buttons').appendChild(button);
 }
